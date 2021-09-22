cda117791bf345e13b3d8d5bbaa7ee8b36b09d93
Nicolas Esdras <nicolas.esdras971@gmail.com>
2021-09-23 01:06:45 +0200

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -45,13 +45,52 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
+def word_count_dict(filename):
+  word_count = {}  # Map each word to its count
+  input_file = open(filename, 'r')
+  for line in input_file:
+    words = line.split()
+    for word in words:
+      word = word.lower()
+      # Special case if we're seeing this word for the first time.
+      if not word in word_count:
+        word_count[word] = 1
+      else:
+        word_count[word] = word_count[word] + 1
+  input_file.close()  # Not strictly required, but good form.
+  return word_count
+    
+
+def print_words(filename):
+  word_count = word_count_dict(filename)
+  words = sorted(word_count.keys())
+  for word in words:
+    print(word, word_count[word])
+    return
+
+def get_count(word_count_tuple):
+  """Returns the count from a dict word/count tuple  -- used for custom sort."""
+  return word_count_tuple[1]
+
+def print_top(filename):
+  word_count = word_count_dict(filename)
+
+  # Each item is a (word, count) tuple.
+  # Sort them so the big counts are first using key=get_count() to extract count.
+  items = sorted(word_count.items(), key=get_count, reverse=True)
+
+  # Print the first 20
+  for item in items[:20]:
+    print(item[0], item[1])
+    return
+
 ###
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print ('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
   option = sys.argv[1]
@@ -61,7 +100,7 @@ def main():
   elif option == '--topcount':
     print_top(filename)
   else:
-    print 'unknown option: ' + option
+    print ('unknown option: ') + option
     sys.exit(1)
 
 if __name__ == '__main__':
